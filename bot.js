f80caa8e173473111200851af0d0c83ccb4ad1a9
Nikola <neparosaurus@gmail.com>
2021-04-09 19:06:03 +0100

    add buy now/add to cart, fix css classes

--- bot.js
+++ bot.js
@@ -3,13 +3,17 @@ const dotenv = require('dotenv');
 const config = dotenv.config();
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox']});
-  const page = await browser.newPage();
-  const timeout = parseInt(process.env.TIMEOUT);
-  const timeout_randomness = parseInt(process.env.TIMEOUT_RANDOMNESS);
-  const max_price = parseFloat(process.env.MAX_PRICE);
-  const product_url = process.env.PRODUCT_URL;
-  let price, symbol;
+  const browser = await puppeteer.launch({headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox']}),
+        page = await browser.newPage(),
+        timeout = parseInt(process.env.TIMEOUT),
+        timeout_randomness = parseInt(process.env.TIMEOUT_RANDOMNESS),
+        max_price = parseFloat(process.env.MAX_PRICE),
+        product_url = process.env.PRODUCT_URL;
+
+  let price,
+    symbol,
+    is_available,
+    has_buying_options;
 
   // Configure browser
   // await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0');
@@ -21,11 +25,13 @@ const config = dotenv.config();
     await page.waitForSelector('#buybox');
 
     const delay = getRandomDelay(timeout, timeout_randomness);
-    const is_available = await page.evaluate(evaluateIsAvailable);
+    is_available = await page.evaluate(evaluateIsAvailable);
 
+    // Is product available to buy
     if (is_available) {
       [price, symbol] = await page.evaluate(evaluateGetPrice, '#newPitchPriceWrapper_feature_div .a-section'); // #newPitchPriceWrapper_feature_div
 
+      // If price is ok, break and add product to cart
       if (price < max_price) {
         break;
       }
@@ -36,25 +42,36 @@ const config = dotenv.config();
 
     }
     else {
-      const has_buying_options = await page.evaluate(evaluateHasBuyingOptions);
+      has_buying_options = await page.evaluate(evaluateHasBuyingOptions);
 
+      // If product has buying options instead of buy now
       if (has_buying_options) {
         await page.click('#buybox-see-all-buying-choices .a-button-text');
-        await page.waitForSelector('#aod-price-1');
+        await page.waitForSelector('#aod-offer-list');
 
-        [price, symbol] = await page.evaluate(evaluateGetPrice, '#aod-price-1 .a-offscreen');
+        // Check if has buying options when options slide from right
+        has_buying_options = await page.evaluate(evaluateHasBuyingOptionsWhenOpen);
 
-        if (price < max_price) {
-          const shippingMessage = await page.evaluate(evaluateShippingMessage);
+        if (!has_buying_options) {
+          log('Not available... (Delay: '+delay+'ms)');
+        }
+        else {
+          [price, symbol] = await page.evaluate(evaluateGetPrice, '#aod-offer #aod-price-1 .a-offscreen');
 
-          if (shippingMessage === null) {
-            break;
+          if (price < max_price) {
+            const shippingMessage = await page.evaluate(evaluateShippingMessage);
+
+            // If no shipping message, break and add product to cart
+            if (shippingMessage === null) {
+              break;
+            }
+
+            log('Found at ' + symbol + '' + price + ' with error: ' + shippingMessage + ' (Delay: ' + delay + 'ms)', 'i');
+          } else {
+            log('Found overpriced at ' + symbol + price + ' (Delay: ' + delay + 'ms)', 'i');
           }
-
-          log('Found at '+symbol+''+price+' with error: '+shippingMessage+' (Delay: '+delay+'ms)', 'i');
         }
 
-        log('Found overpriced at '+symbol+price+' (Delay: '+delay+'ms)', 'i');
         await page.waitForTimeout(delay);
 
       } else {
@@ -66,7 +83,17 @@ const config = dotenv.config();
 
   log('Found at '+symbol+price+'!', 's');
 
-  // TODO: Buy product
+  // Buy product
+  if (is_available) {
+    // Buy now
+    await page.click('#buy-now-button');
+  } else if (has_buying_options) {
+    // Add to cart
+    await page.click('#aod-offer-list > div:first-of-type [name="submit.addToCart"]');
+  }
+
+  await page.waitForNavigation();
+  await page.click('[name=data-feature-id="proceed-to-checkout-action"]');
 
   // await browser.close();
 })();
@@ -108,6 +135,10 @@ evaluateIsShipping = async () => {
 
 evaluateHasBuyingOptions = async () => {
   return document.querySelector('#buybox-see-all-buying-choices') !== null
+}
+
+evaluateHasBuyingOptionsWhenOpen = async () => {
+  return document.querySelector('#aod-offer-list > div') !== null
 }
 
 evaluateGetPrice = async (selector) => {
